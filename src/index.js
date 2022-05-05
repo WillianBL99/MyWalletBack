e7678027fa8cdb322aae591e3cb75589a0518fa6
paulo uilian <uilianlbarros@gmail.com>
2022-05-05 15:56:45 -0300

    Primeiro commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import express from "express";
+import cors from 'cors';
+import chalk from "chalk";
+import dotenv from 'dotenv'
+dotenv.config();
+
+const app = express()
+app.use(cors());
+
+const port = process.env.PORT;
+
+app.listen(port, () => {
+    console.log(chalk.bold.green(`Server is running on port/${port}`));
+});
