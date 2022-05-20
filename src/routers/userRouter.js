b9838a004df425f5ab3bbe92962105cdedf9ed12
Rodrigodcs <rodrigodcsee@gmail.com>
2022-05-20 12:09:30 -0300

    Preparando revisão de código

--- src/routers/userRouter.js
+++
@@ -1,9 +0,0 @@
-import {Router} from 'express';
-import { signIn, signUp } from '../controllers/userController.js';
-
-const userRouter = Router();
-
-userRouter.post("/sign-up", signUp);
-userRouter.post("/sign-in", signIn);
-
-export default userRouter;
